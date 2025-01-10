53ae01c2286959c67fa18cb37030b9e61daee7c4
Roméo Poirier <romeo.poirier@epitech.eu>
2025-01-11 00:59:51 +0100

    fix(my_str.h) removed file names from documentation of funcs

--- lib/my/headers/my_str.h
+++ lib/my/headers/my_str.h
@@ -8,7 +8,8 @@
 #ifndef MY_STR_H
     #define MY_STR_H
 
-    //my_*len.c
+//my_*len.c
+
 //return the length of str    
 int my_strlen(char const *str);
 //return the length from n to the first encountered character that is in c
@@ -19,12 +20,14 @@ int my_intlen(int nb);
 int my_llilen(long long int nb);
 
 
-    //my_findstr.c
+//my_findstr.c
+
 //return the first index where to_find was found in str or -1 if not found
 int my_strstr(char *str, char const *to_find);
 
 
-    //my_is*.c
+//my_is*.c
+
 //return 1 if char is a letter or a number
 int my_char_isalnum(char const c);
 //return 1 if char is a letter
@@ -52,26 +55,28 @@ int my_str_isprint(char const *str);
 //return 1 if str only consists of uppercase letters
 int my_str_isupper(char const *str);
 
+//my_put*.c
 
-    //my_put*.c
-//display a character in the standard output
+//display a character in the standard output, return 1
 int my_putchar(char const c);
-//display an int in the standard output
+//display an int in the standard output, return length of nb
 int my_putint(int nb);
-//display a long long int in the standard output
+//display a long long int in the standard output, return length of nb
 int my_putlli(long long int nb);
-//display str in the standard output
+//display str in the standard output, return length of str
 int my_putstr(char const *str);
-//display str from i to end in the standard output
+//display str from i to end in the standard output, return end - i
 int my_putstr_i_end(char const *str, int i, int end);
 
 
-    //my_revstr.c
+//my_revstr.c
+
 //reverse str
 char *my_revstr(char *str);
 
 
-    //my_str| capitalize | upcase | lowcase |.c
+//my_str| capitalize | upcase | lowcase |.c
+
 //the first letter of every word becomes uppercase
 char *my_strcapitalize(char *str);
 //all letters become lowercase
@@ -80,18 +85,21 @@ char *my_strlowcase(char *str);
 char *my_strupcase(char *str);
 
 
-    //my_str_to_warr.c
+//my_str_to_warr.c
+
 //turn str into a malloc'd word array
 char **str_to_warr(char *str);
 //free a word array
 void free_warr(char **warr);
 
-    //my_nb_words.c
+//my_nb_words.c
+
 //return the number of words in str
 int my_nb_words(char *str);
 
 
-    //my_strcat.c
+//my_strcat.c
+
 /*concatenates src at the end of dest.
 dest needs to be of correct size*/
 char *my_strcat(char *dest, char const *src);
@@ -103,7 +111,8 @@ dest needs to be of correct size*/
 char *mstrn_to_mcat(char *dest, char const *src, int n, int m);
 
 
-    //my_strcmp.c
+//my_strcmp.c
+
 /*compare s1 and s2, return the difference s1's char - s2's char using ascii.
 return 0 if no different characters encountered*/
 int my_strcmp(char const *s1, char const *s2);
@@ -112,7 +121,8 @@ using ascii. return 0 if no different characters encountered*/
 int my_strncmp(char const *s1, char const *s2, int n);
 
 
-    //my_strcpy.c
+//my_strcpy.c
+
 /*copy src into dest.
 dest needs to be of correct size*/
 char *my_strcpy(char *dest, char const *src);
@@ -127,7 +137,8 @@ dest needs to be of correct size*/
 char *mstrn_to_mcpy(char *dest, char const *src, int n, int m);
 
 
-    //my_tab_len.c
+//my_tab_len.c
+
 //return the number of items in tab
 int char_tab_len(char **tab);
 #endif /* MY_STR_H */
